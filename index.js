0cde2876676a26fb5a327a32a511e25a57ff5fc4
ChauMing <971334483@qq.com>
2017-04-16 10:52:53 +0800

    style: 分离 eval 和执行 eval

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-let evaluator = require('./lib/eval');
+'use strict';
 
-module.exports = evaluator;
+// 和 js 的 eval 冲突了......
+const parse = require('./lib/parse');
+const _eval = require('./lib/eval');
+const env = require('./lib/lib');
+
+module.exports = function(code) {
+  let ast = parse(code);
+  let results = ast.map((item) => {
+    return _eval(item, env);
+  });
+  return results.pop().toString();
+}
